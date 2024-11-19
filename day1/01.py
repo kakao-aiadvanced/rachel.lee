4ef0765f05328b96c2a53b2d75323c97b8242090
2pearlmoss@gmail.com <rachel.lee@kakaocorp.com>
2024-11-19 09:35:39 +0900

    day1

---
+++ day1/01.py
@@ -0,0 +1,25 @@
+import config
+from openai import OpenAI
+client = OpenAI()
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": 
+         """
+         Translate the given word into Korean.
+         Apple: 사과,
+         Banana: 바나나,
+         Castle: 성,
+         Dawn: 새벽,
+         Event: 행사
+         """
+         },
+        {
+            "role": "user",
+            "content": "Dog: "
+        }
+    ]
+)
+
+print(completion.choices[0].message)
